23a48da2d17082697dffd8118ee8779314db4a90
Tempystral <elijah.doern@gmail.com>
2019-12-08 12:58:43 -0500

    Update logger_setup.py

--- utils/logger_setup.py
+++ utils/logger_setup.py
@@ -13,16 +13,16 @@ class LoggerSetup:
         self.logger = logging.getLogger('discord')
         self.logformatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s: %(message)s')
         self.handler_file = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='a')
-        self.handler_debug = logging.FileHandler(filename='discord.debug.log', encoding='utf-8', mode='a')
+        #self.handler_debug = logging.FileHandler(filename='discord.debug.log', encoding='utf-8', mode='a')
         self.handler_shell = logging.StreamHandler(stream=sys.stdout)
         self.__setup()
 
     def __setup(self):
         # Base logger sees all levels
-        self.logger.setLevel(logging.DEBUG)
+        self.logger.setLevel(logging.INFO)
         # Special logs will see all logging levels
-        self.handler_debug.setFormatter(self.logformatter)
-        self.handler_debug.addFilter(MinLevelFilter(logging.DEBUG))
+        #self.handler_debug.setFormatter(self.logformatter)
+        #self.handler_debug.addFilter(MinLevelFilter(logging.DEBUG))
         # File writer will see all logging levels
         self.handler_file.setFormatter(self.logformatter)
         self.handler_file.addFilter(MinLevelFilter(logging.INFO))
@@ -30,7 +30,7 @@ class LoggerSetup:
         self.handler_shell.setFormatter(self.logformatter)
         self.handler_file.addFilter(MinLevelFilter(logging.WARNING))
 
-        self.logger.addHandler(self.handler_debug)
+        #self.logger.addHandler(self.handler_debug)
         self.logger.addHandler(self.handler_file)
         self.logger.addHandler(self.handler_shell)
 
