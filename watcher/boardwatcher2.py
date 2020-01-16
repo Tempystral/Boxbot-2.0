09cf605a35a261b127d84630f1ccb6e51eb03145
Tempystral <elijah.doern@gmail.com>
2020-01-15 21:19:40 -0500

    Revert "Revert "Allow boxbot to respond from any server where it's deployed and make start coroutine compatible with python 3.6""
    This reverts commit cba54fa4fe7134a79d52b55a9b60793682b7ca6d.

--- watcher/boardwatcher2.py
+++ watcher/boardwatcher2.py
@@ -1,7 +1,12 @@
-import re, os, aiohttp
+import re
+import os
+
+import aiohttp
+import anyconfig
 
 from watcher import fourchanaio as fourchan
 from utils import logger
+from watcher import fourchanaio as fourchan
 
 class BoardWatcher:
 	def __init__(self, patternfile, regex):
@@ -14,7 +19,7 @@ class BoardWatcher:
 		self._patterns = {}
 		self._exclude_patterns = {}
 		self._boards = set()
-		self._tracked_threads = {}
+		self._tracked_threads = self.loadTrackedThreads()
 		try:
 			self.load_patterns(self._patternFile)
 		except FileNotFoundError:
@@ -33,6 +38,7 @@ class BoardWatcher:
 		for board in self._boards:
 			newMatchedThreads = await self.update_board(board)
 			newThreads.extend(newMatchedThreads)
+		self.saveTrackedThreads()
 		return newThreads
 
 	async def update_board(self, board):
@@ -151,6 +157,22 @@ class BoardWatcher:
 		for board in self._boards:
 			for thread in self._tracked_threads[board]:
 				print(self._tracked_threads[board][thread].url)
+
+	def saveTrackedThreads(self):
+		tracked_threads = {board: {
+			post_id: (post._data, post.board) for post_id, post in self._tracked_threads[board].items()
+		} for board in self._tracked_threads}
+		anyconfig.dump(tracked_threads, "./watcher/tracked_threads.cache", ac_parser="json")
+
+	def loadTrackedThreads(self):
+		try:
+			tracked_threads = anyconfig.load("./watcher/tracked_threads.cache", ac_parser="json")
+			return {board: {
+				# json only supports strings as keys, so post_id must be converted back to an int
+				int(post_id): fourchan.Post(*post) for post_id, post in tracked_threads[board].items()
+			} for board in tracked_threads}
+		except (FileNotFoundError, ValueError):
+			return {}
 	
 	
 
